f4069ed77dab93a632a2dcdf1eccc33384c2cab0
btrosso <51491995+btrosso@users.noreply.github.com>
2022-02-17 21:27:39 -0600

    update main
    added a line to increase the ball speed every time it hits the paddle.

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ screen.setup(width=800, height=600)
 screen.bgcolor("black")
 screen.title("Rosso's Pong Game!")
 screen.tracer(0)
+sleep_time = 0.1
 
 scoreboard = Scoreboard()
 
@@ -27,7 +28,7 @@ screen.onkeypress(fun=l_paddle.move_down, key="s")
 
 
 while game_is_on:
-    time.sleep(0.1)
+    time.sleep(sleep_time)
     screen.update()
     ball.move()
 
@@ -43,14 +44,20 @@ while game_is_on:
     if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
         ball.bounce_x()
 
+        # if you want the ball to move faster every time it hits a paddle then uncomment the line below
+        # sleep_time -= .005
+
+    # Detect out of bounds on right
     if ball.xcor() > 380:
         ball.reset_position()
         scoreboard.update_l_score()
 
+    # Detect out of bounds on left
     if ball.xcor() < -380:
         ball.reset_position()
         scoreboard.update_r_score()
 
+    # Game over
     if scoreboard.r_score > 0 or scoreboard.l_score > 0:
         game_is_on = False
         scoreboard.game_over()
